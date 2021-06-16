6feab461cedf203f109ac647e1c6ce5098c21d09
zlotawanna@gmail.com <zlotawanna@gmail.com>
2021-06-16 13:46:19 +0200

    Add user stocks table on portfolio page

--- db/migrate/20210616103421_create_user_stocks.rb
+++ db/migrate/20210616103421_create_user_stocks.rb
@@ -1,8 +1,8 @@
 class CreateUserStocks < ActiveRecord::Migration[6.0]
   def change
     create_table :user_stocks do |t|
-      t.resources :user, null: false, foreign_key: true
-      t.resources :stock, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :stock, null: false, foreign_key: true
 
       t.timestamps
     end
