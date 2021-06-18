ad9f0b742a3ff8c0982d57743d695633f5b30e3d
zlotawanna@gmail.com <zlotawanna@gmail.com>
2021-06-18 09:38:14 +0200

    Create friendship model, add search friend function, ajax for result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,32 @@
 class UsersController < ApplicationController
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+    @user_friends = current_user.friends
+  end
+
+  def search
+    if params[:user].present?
+      @friend = User.check_db(params[:user])
+        if @friend
+          respond_to do |format|
+            format.js { render partial: 'users/friend_result' }
+          end
+        else
+          respond_to do |format|
+            flash.now[:alert] = "No such a user"
+            format.js { render partial: 'users/friend_result' }
+          end
+        end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter an email first"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -24,4 +26,9 @@ class User < ApplicationRecord
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !already_track_stock?(ticker_symbol)
   end
+
+  def self.check_db(email)
+    where(email: email.downcase).first
+  end
+
 end
